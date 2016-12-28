754ac9d7ba1177548ad40a7cb98d2a0d2b9c16ca
Hakeem Green <hvk33m@Hakeems-MacBook-Pro.local>
2016-12-27 20:33:41 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  context: __dirname + "",
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/js/scripts.js",
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/, 
+        exclude: /(node_modules|bower_components)/, 
+        loader: "babel-loader",
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        } 
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/js",
+    publicPath: "/src/js",
+    filename: "client.min.js"
+  },
+  devServer:{
+    contentBase: __dirname,
+    historyApiFallback: true
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};
